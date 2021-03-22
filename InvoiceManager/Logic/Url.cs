17ebc2c043e81b07a78fbf2d1ea1e5bfd2e7f1ec
parysoid <jakub.parys@seznam.cz>
2021-03-22 17:28:40 +0100

    Upravené přidání výplaty

--- InvoiceManager/Logic/Url.cs
+++ InvoiceManager/Logic/Url.cs
@@ -7,6 +7,23 @@ namespace InvoiceManager.Logic
 {
     public class Url
     {
-        
+        public static bool HasId (string url)
+        {
+            int id = 0;
+
+            if (Int32.TryParse(url.Substring(url.LastIndexOf("/") + 1), out id))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static int GetId(string url)
+        {
+            return Convert.ToInt32(url.Substring(url.LastIndexOf("/") + 1));
+        }
     }
 }
