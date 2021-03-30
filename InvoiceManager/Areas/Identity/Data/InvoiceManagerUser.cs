e1ffb950ea6e77096a4993dc3312b52ec7bbadd8
parysoid <jakub.parys@seznam.cz>
2021-03-30 10:54:09 +0200

    Přidáno Identity

---
+++ InvoiceManager/Areas/Identity/Data/InvoiceManagerUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace InvoiceManager.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the InvoiceManagerUser class
+    public class InvoiceManagerUser : IdentityUser
+    {
+    }
+}
